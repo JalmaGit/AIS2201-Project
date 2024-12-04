cb473b3b93c58b12d3020f8ae1a2bf250753a1d1
Jalma <jalma@83-243-174-21.fth.tafjordconnect.net>
2024-12-04 06:29:06 +0100

    Made main work

--- python/main.py
+++ python/main.py
@@ -1,8 +1,32 @@
 from detection import detection_algorithms as da
 from signal_generation import signal_generation as sg
+import matplotlib.pyplot as plt
+
 
 
 fs = 48000
 signals = sg()
 
+sampling_freq = 48000
+time_in_seconds = 6
+samples = sampling_freq*time_in_seconds
+frequency = 400
+Amplitude = 1
+
+pure_tone, time = sg.generate_pure_sinewave(sampling_freq, samples, frequency, Amplitude)
+noisy_pure_tone = sg.generate_noisy_signal(pure_tone,1000, Amplitude*10)
 detect = da(fs)
+
+bufferSize = 4096 #Samples in buffer
+
+freq_est = []
+time_est = [0]
+
+for index in range(len(noisy_pure_tone)//bufferSize):
+    f_est, t = detect.simple_detection(noisy_pure_tone[index*bufferSize:index*bufferSize+bufferSize])
+
+    freq_est.append(f_est)
+    time_est.append(t+time_est[-1])
+
+plt.plot(time_est[1:], freq_est)
+plt.show()

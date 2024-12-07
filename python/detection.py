578de908e711b68e8d0be71b29533f70767150e6
Jalma <jalma@83-243-166-11.fth.tafjordconnect.net>
2024-12-07 05:57:55 +0100

    added voices

--- python/detection.py
+++ python/detection.py
@@ -21,3 +21,23 @@ class detection_algorithms:
         f_est = np.argmax(Xm)*self.fs/N
 
         return f_est, N/self.fs
+
+    def lowpass_filter(self, sample):
+        N = len(sample)
+
+
+    def moving_average(self, sample):
+        pass
+
+    def autocorrelation_detection(self, sample):
+        N = len(sample)
+        autocorr = np.correlate(sample, sample, mode='full')  # Compute full autocorrelation
+        autocorr = autocorr[N:]  # Take the second half
+        peaks = np.where((autocorr[:-1] < autocorr[1:]) & (autocorr[1:] > autocorr[2:]))[0]
+
+        if len(peaks) > 0:
+            f_est = self.fs / peaks[0]
+        else:
+            f_est = 0  # No clear fundamental frequency
+
+        return f_est, N / self.fs
